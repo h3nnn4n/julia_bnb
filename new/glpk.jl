f5fe6a03ec7fb4c0f10b457dfdc486cb4974375e
Renan Samuel da Silva <uber.renan@gmail.com>
2015-08-13 19:12:43 -0300

    Wip, branching, doKnapSack...

--- new/glpk.jl
+++ new/glpk.jl
@@ -1,78 +1,143 @@
 using GLPK
+using DataStructures
 
-type instance
-    weight   :: Array{Int}
-    profit   :: Array{Int}
-    size     :: Int
-    capacity :: Int
-
-    x        :: Array{Float64}
-
-    obj      :: Float64
+type Restriction
+    boundPos     :: Int
+    boundType    :: Int  # -1 is lessThan and +1 is greaterThan, 0 is nothing
 end
+
+type Instance
+    weight       :: Array{Int}
+    profit       :: Array{Int}
+    size         :: Int
+    capacity     :: Int
+
+    x            :: Array{Float64}
+
+    restrictions :: Array{Restriction}
+
+    obj          :: Float64
+end
+
+Base.isless   (x :: Instance, y :: Instance) = (x.obj) < (y.obj)
+Base.isless   (x :: Instance, y)             = (x.obj) < (y)
+
+Base.copy          (x :: Instance)           = Instance(copy(weight), copy(profit), copy(size), copy(capacity), copy(x), copy(restrictions), copy(obj))
+Base.copy          (x :: Restriction)        = Restriction(copy(boundPos), copy(boundType))
 
 @everywhere function isInt(n)
     return abs(n) == trunc(abs(n))
 end
 
-@everywhere function newKnapSack(size)
-    return instance(rand(1:size, size), rand(1:size, size), size, size, [],  -1.0)
+@everywhere function isSolved(w :: Instance)
+    for i in 1:w.size
+        if !isInt(w.x[i])
+            return false
+        end
+    end
+
+    return true
 end
 
-@everywhere function doKnapSack(instance)
-    weight   = instance.weight
-    profit   = instance.profit
-    capacity = instance.capacity 
+@everywhere function newKnapSack(size :: Int)
+    return Instance(rand(1:size, size), rand(1:size, size), size, size, [], [],  -1.0)
+end
 
-    lp = GLPK.Prob()
+@everywhere function doKnapSack(w :: Instance)
+    weight   = w.weight
+    profit   = w.profit
+    capacity = w.capacity 
+
+    lp       = GLPK.Prob()
 
     GLPK.set_prob_name(lp, "knapsack")
     GLPK.set_obj_dir(lp, GLPK.MAX)
 
     GLPK.add_rows(lp, 1)
     GLPK.set_row_name(lp, 1, "p")
-    GLPK.set_row_bnds(lp, 1, GLPK.UP, 0.0, instance.capacity)
+    GLPK.set_row_bnds(lp, 1, GLPK.UP, 0.0, w.capacity)
 
-    GLPK.add_cols(lp, instance.size)
+    GLPK.add_cols(lp, w.size)
 
-    for i in 1:instance.size
-        GLPK.set_obj_coef(lp, i, instance.profit[i])
-        
+    for i in 1:w.size
+        GLPK.set_obj_coef(lp, i, w.profit[i])
+
         GLPK.set_col_bnds(lp, i, GLPK.DB, 0.0, 1.0)
     end
 
-    A = [ instance.weight[i] for i in 1:instance.size ]
+    A = [ w.weight[i] for i in 1:w.size ]
 
     GLPK.load_matrix(lp, sparse(transpose(A)))
 
+    for i in 1:size(w.restrictions)[1]
+        if w.restrictions.boundType == 1
+            GLPK.set_col_bnds(lp, w.restrictions.boundPos, GLPK.UP, ceil(w.x[i]))
+        elseif w.restrictions.boundType == -1
+            GLPK.set_col_bnds(lp, w.restrictions.boundPos, GLPK.LO, floor(w.x[i]))
+        end
+    end
+
     simplex(lp)
 
-    instance.obj = GLPK.get_obj_val(lp)
+    w.obj = GLPK.get_obj_val(lp)
 
-    #=println("\n\n")=#
-    #=println("Capacity: $capacity")=#
-    #=println("Profit:   $profit")=#
-    #=println("Weight:   $weight")=#
-    #=println("Obj F:    ", GLPK.get_obj_val(lp))=#
+    w.x = [ GLPK.get_col_prim(lp, i) for i in 1:w.size]
 
-    #=print  ("x[i]:     ")=#
+    return w
+end
 
-    #=for i in 1:size=#
-        #=print(GLPK.get_col_prim(lp, i), " ")=#
-    #=end=#
+@everywhere function branchDown(w :: Instance, pos :: Int, typ :: Int)
+    push!(w.restrictions, Restriction(pos, typ))
 
-    instance.x = [ GLPK.get_col_prim(lp, i) for i in 1:instance.size]
+    q = doKnapSack(w)
 
-    return instance
+    return q
+end
+
+@everywhere function branchUp(w :: Instance)
+
 end
 
 function main()
+    
+    lp = doKnapSack(newKnapSack(10)) 
 
-    knapSack = newKnapSack(10000)
+    heap = binary_maxheap(Instance)
 
-    lp = doKnapSack(knapSack) 
+    push!(heap, lp)
 
-    simplex(lp)
+    best = 0.0
+    bestSol = (lp)
 
+    while length(heap) > 0
+        w = pop!(heap)
+        println(w)
+
+        if isSolved(w)
+           if w.obj > best
+               best    = w.obj
+               bestSol = w
+           end
+        else
+            for i in 1:w.size
+                if !isInt(w.x[i])
+                    println("here")
+                    down = branchDown( (w), i, -1 )
+                    up   = branchUp  ( (w), i, +1 )
+
+                    if up > best
+                        best    = up.obj
+                        bestSol = up
+                    end
+
+                    if down > best
+                        best    = down.obj
+                        bestSol = down
+                    end
+                    break
+                end
+            end
+        end
+    end
 end
 
